017ea9168a43c333b75fa225381dec174f278566
Joseph Lee <josephlee498@gmail.com>
2025-01-30 09:56:38 +0000

    initial commit

---
+++ src/main/java/com/nmscinemas/nms_cinemas_backend/NmsCinemasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nmscinemas.nms_cinemas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmsCinemasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmsCinemasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nmscinemas/nms_cinemas_backend/NmsCinemasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmscinemas.nms_cinemas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmsCinemasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
